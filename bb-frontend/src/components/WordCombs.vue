763aac12e0d175d7871c5a1eb3d0dd3e8369e7b7
pytherik <erikberndt@gmx.net>
2024-06-06 22:59:00 +0200

    second commit

---
+++ bb-frontend/src/components/WordCombs.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const { targetLetters } = defineProps(['targetLetters'])
+const emits = defineEmits(['addLetter'])
+
+</script>
+
+<template>
+<div class="comb">
+  <div class="target-letters">
+    <div class="target-letter" @click="emits('addLetter', targetLetters[0])">{{ targetLetters[0] }}</div>
+    <div class="target-letter" @click="emits('addLetter', targetLetters[1])">{{ targetLetters[1] }}</div>
+  </div>
+  <div class="target-letters">
+    <div class="target-letter" @click="emits('addLetter', targetLetters[2])">{{ targetLetters[2] }}</div>
+    <div class="target-letter required" @click="emits('addLetter', targetLetters[3])">{{ targetLetters[3] }}</div>
+    <div class="target-letter" @click="emits('addLetter', targetLetters[4])">{{ targetLetters[4] }}</div>
+  </div>
+  <div class="target-letters">
+    <div class="target-letter" @click="emits('addLetter', targetLetters[5])">{{ targetLetters[5] }}</div>
+    <div class="target-letter" @click="emits('addLetter', targetLetters[6])">{{ targetLetters[6] }}</div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.comb {
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.target-letters {
+  width: 20rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.target-letter {
+  user-select: none;
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  border: 1px solid #dedeed;
+  border-radius: 1.5rem;
+}
+
+.required {
+  color: var(--required-letter-color);
+}
+
+
+</style>
--- bb-frontend/src/views/HomeView.vue
+++ bb-frontend/src/views/HomeView.vue
@@ -2,9 +2,9 @@
 import { words } from '@/assets/deutsch';
 import { targetWords } from '@/assets/targetWords';
 import { ref } from 'vue';
+import WordCombs from "@/components/WordCombs.vue";
 
 const guess = ref('');
-const guessLetters = ref([]);
 const requiredLetter = ref('');
 const randomWord = ref('');
 const targetLetters = ref<string[]>([]);
@@ -12,42 +12,37 @@ const possibleIncludes = ref(0);
 const includes = ref<string[]>([]);
 const score = ref(0);
 const knownWords = ref<string[]>([]);
+
 randomWord.value = targetWords[Math.floor(Math.random() * targetWords.length)].toUpperCase();
 targetLetters.value = Array.from(new Set(randomWord.value.split('')));
-requiredLetter.value = targetLetters.value[Math.floor(Math.random() * targetLetters.value.length)];
-
 shuffle(targetLetters.value);
+requiredLetter.value = targetLetters.value[3];
 
-countPossibleIncludes();
+collectPossibleIncludes();
 
-function countPossibleIncludes() {
+function collectPossibleIncludes() {
   words.forEach((word) => {
     const wordLetters = Array.from(new Set(word.toUpperCase().split('')));
     const res = wordLetters.some((letter) => !targetLetters.value.includes(letter));
-    if (!res && wordLetters.some((letter) => letter === requiredLetter.value)) {
+    if (!res && wordLetters.some((letter) => letter === requiredLetter.value) && !includes.value.includes(word.toUpperCase())) {
       possibleIncludes.value++;
-      includes.value.push(word);
+      includes.value.push(word.toUpperCase());
     }
   });
-  console.log(includes.value);
 }
 
 function checkWord(e: any) {
   e.preventDefault();
   if (
-    (includes.value.includes(guess.value) ||
-      includes.value.includes(guess.value[0].toUpperCase() + guess.value.slice(1))) &&
-    !knownWords.value.includes(guess.value) &&
-    !knownWords.value.includes(guess.value[0].toUpperCase() + guess.value.slice(1))
+    includes.value.includes(guess.value.toUpperCase()) &&
+    !knownWords.value.includes(guess.value.toUpperCase())
   ) {
     score.value += guess.value.length > 4 ? guess.value.length : 1;
-    knownWords.value.push(guess.value);
+    knownWords.value.push(guess.value.toUpperCase());
+    knownWords.value.sort();
+    possibleIncludes.value--;
   }
   guess.value = '';
-}
-
-function allLettersIncluded(): boolean {
-  return !guessLetters.value.some((letter) => !targetLetters.value.includes(letter));
 }
 
 function shuffle(array: string[]) {
@@ -58,49 +53,92 @@ function shuffle(array: string[]) {
     [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
   }
 }
+
+function addLetterToGuess(letter: string) {
+  guess.value += letter;
+}
+
+function removeLetterFromGuess() {
+  guess.value = guess.value.slice(0,-1);
+}
 </script>
 
 <template>
   <main>
-    <h1>Hallo</h1>
-    <div class="target-letters">
-      <div class="target-letter" v-for="letter in targetLetters" :key="letter">
-        <span :class="{ required: letter === requiredLetter }">{{ letter }}</span>
-      </div>
-    </div>
-    <div v-if="includes.length > 0">
-      <h2>Mögliche Wörter: {{ includes.length }}</h2>
-      <!--      <ul>-->
-      <!--        <li v-for="word in includes" :key="word">{{ word }}</li>-->
-      <!--      </ul>-->
-    </div>
-    <div>Du hast schon {{ knownWords.length }} Wörter.</div>
-    <div>Deine Punkte: {{ score }}</div>
+    <div class="playground">
+    <div class="score">{{ score }} Punkte</div>
+    <WordCombs :targetLetters="targetLetters"  @addLetter="addLetterToGuess" />
+    <form @submit="checkWord">
+      <label for="guess"></label>
+      <input type="text" placeholder="nächstes Wort" size="12" v-model="guess" />
+      <button type="button" class="btn btn-delete" @click="removeLetterFromGuess">«</button>
+      <button type="submit" class="btn btn-submit">OK</button>
+    </form>
+    <div class="num-words">{{ knownWords.length }}/{{ includes.length }} Wörtern</div>
     <ul>
       <li v-for="word in knownWords" :key="word">{{ word }}</li>
     </ul>
-    <form @submit="checkWord">
-      <label for="guess">Wort</label><br />
-      <input type="text" v-model="guess" />
-      <button>OK</button>
-    </form>
+    </div>
   </main>
 </template>
 
 <style scoped>
-.target-letters {
-  width: 20rem;
+.playground {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: flex-start;
   align-items: center;
 }
 
-.target-letter {
-  font-size: 2rem;
-  border: 1px solid #dedeed;
+.score {
+  font-size: 3rem;
 }
 
-.required {
-  color: #883377;
+.num-words {
+  font-size: 2rem;
+  margin: 1rem 0;
+}
+
+ul {
+  width: 40rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+li {
+  list-style: none;
+  margin: .2rem;
+  padding: 0 .3rem;
+  border: 1px solid var(--required-letter-color);
+  border-radius: 2rem;
+}
+
+input {
+  background-color: transparent;
+  font-size: 2rem;
+  outline: none;
+  border: none;
+  border-bottom: 1px solid #dedeed;
+  color: #dedeed;
+}
+
+.btn {
+  background: transparent;
+  color: #dedeed;
+}
+
+.btn-submit {
+  padding: .5rem .4rem;
+  border: 1px solid var(--color-green);
+  border-radius: 100%;
+}
+
+.btn-delete {
+  padding: .4rem .7rem;
+  border: 1px solid var(--color-red);
+  border-radius: 100%;
+  margin-right: .5rem;
 }
 </style>
